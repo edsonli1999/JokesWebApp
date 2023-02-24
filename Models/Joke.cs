8549c6ef5b13b24e93c60bdeafd69b67e89b014d
yangyueedson <yangyueedson@student.unimelb.edu.au>
2023-02-24 16:59:52 +0800

    Added joke.cs class and its properties, need to create controller next

---
+++ Models/Joke.cs
@@ -0,0 +1,15 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswe { get; set; }
+
+        // ctor -> shortcut to create constructor
+        public Joke()
+        {
+
+        }
+    }
+}
